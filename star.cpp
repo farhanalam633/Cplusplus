e8f70a6562da7cd549a929a11c12d50ba91044a5
F ∆ R H ∆ N__∆ L ∆ M <farhanalam.dev@gmail.com>
2022-02-05 00:11:29 +0530

    Diamond pattern

--- star.cpp
+++ star.cpp
@@ -8,17 +8,20 @@ int main(){
     cin>>n;
 
     for(int i=1; i<=n; i++){
-        int j;
-        for(j=1; j<=n-i; j++){
+        for(int j=1; j<=n-i; j++){
             cout<<"  ";
         }
-        int k=i;
-        for(; j<=n; j++){
-            cout<<k--<<" ";
+        for(int j=1; j<=2*i-1; j++){
+            cout<<" *";
         }
-        k=2;
-        for(; j<=n+i-1; j++){
-            cout<<k++<<" ";
+        cout<<endl;
+    }
+    for(int i=n; i>=1; i--){
+        for(int j=1; j<=n-i; j++){
+            cout<<"  ";
+        }
+        for(int j=1; j<=2*i-1; j++){
+            cout<<" *";
         }
         cout<<endl;
     }
